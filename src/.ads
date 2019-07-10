c998f325fb41dbea170c1345c6f439c7e3a3571c
ysykzheng <zhengminghai@new4g.cn>
2019-07-10 11:12:19 +0800

    public dadapush client

---
+++ src/.ads
@@ -0,0 +1,14 @@
+--  DaDaPush Public API
+--  DaDaPush: Real_time Notifications App Send real_time notifications through our API without coding and maintaining your own app for iOS or Android devices.
+--  ------------ EDIT NOTE ------------
+--  This file was generated with openapi-generator.  You can modify it to implement
+--  the server.  After you modify this file, you should add the following line
+--  to the .openapi-generator-ignore file:
+--
+--  src/.ads
+--
+--  Then, you can drop this edit note comment.
+--  ------------ EDIT NOTE ------------
+package  is
+
+end ;
